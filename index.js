f5c5dfc37eff475786709fd02704456f30f34c38
victor jardes <victor_maanoel@outlook.com>
2022-07-01 15:37:43 -0400

    build(index.js): import route and using route in app

--- index.js
+++ index.js
@@ -2,15 +2,14 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const bodyParse = require('body-parser');
+const routes = require('./routes');
 
 const app = express();
 app.use(cors());
 app.use(bodyParse.json());
-
 const { PORT } = process.env;
 
-const port = PORT;
-
 app.get('/', (req, res) => res.send('Hello World!'));
+app.use(routes.getAllTaskRouter);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
