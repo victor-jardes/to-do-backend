ca22ef8740ac2919e52ce0a2badcdf68417bd853
victor jardes <victor_maanoel@outlook.com>
2022-07-01 00:00:43 -0400

    refactor(task.sql): add correct querys

--- Task.sql
+++ Task.sql
@@ -2,18 +2,20 @@ DROP SCHEMA IF EXISTS Task;
 
 CREATE SCHEMA IF NOT EXISTS Task;
 
-CREATE TABLE To_do(
-  id INTERGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
-  work TEXT NOT NULL,
-  `status` TEXT NOT null
-)
+CREATE TABLE Task.To_do(
+  id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+ `work` TEXT NOT NULL,
+  `status` TEXT NOT NULL
+);
 
 INSERT INTO
-  Task (wok, `status`)
+  Task.To_do (`work`, `status`)
 VALUES 
   ("tem que praticar o front", "feito!");
 
 INSERT INTO
-  Task (wok, `status`)
+  Task.To_do (`work`, `status`)
 VALUES 
   ("ja fez o DUO hoje?", "pendente...");
+  
+ 
