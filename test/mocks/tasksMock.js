0267fa86b12801b5a0c7a5341ea621c1fef03559
victor jardes <victor_maanoel@outlook.com>
2022-07-01 21:26:42 -0400

    feat(test/mocks/taskmock.js): create file test and moch for using in test
    n

---
+++ test/mocks/tasksMock.js
@@ -0,0 +1,15 @@
+module.exports = {
+  empty: [],
+  full: [
+    {
+      id: 1,
+      work: 'tem que praticar o front',
+      status: 'feito',
+    },
+    {
+      id: 2,
+      work: 'ja fez o DUO hoje?',
+      status: 'pendente',
+    },
+  ],
+};
