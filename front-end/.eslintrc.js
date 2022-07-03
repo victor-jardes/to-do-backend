366090e14f326d8797c96bc214b90e70a0afdbc3
victor jardes <victor_maanoel@outlook.com>
2022-07-02 21:17:11 -0400

    refactor(front-end): chage file names and add prettier and chanfe eslint config

--- front-end/.eslintrc.js
+++ front-end/.eslintrc.js
@@ -1,27 +1,29 @@
+// .eslintrc.js
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
-  },
-  'jsx-runtime': {
-    plugins: [
-      'react',
-    ],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
   },
 };
